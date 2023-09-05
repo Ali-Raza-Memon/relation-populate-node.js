147d859793d7a4623fc9ebe6652ba9e6ff922961
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-09-05 11:31:52 +0500

    StaffController is created!

---
+++ controllers/StaffController.js
@@ -0,0 +1,17 @@
+ const Staff = require('../models/staffModels');
+
+ module.exports ={
+
+    create: async(req,res) =>{
+        
+        const {name,email} = req.body;
+        const staff = await Staff.create({
+       
+            name,
+            email
+        
+        });
+
+        return res.send(staff);
+    }
+ }
