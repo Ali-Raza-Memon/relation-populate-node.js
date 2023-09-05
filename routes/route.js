c1e7d31fe573276b6a33d71948d048239e7c203b
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-09-05 11:47:11 +0500

    routes are created and now testing apis

--- routes/route.js
+++ routes/route.js
@@ -1,1 +1,10 @@
-  
+const express = require('express');
+const router = new express.Router;
+const Staff = require('../controllers/StaffController');
+const Right = require('../controllers/RightController');
+
+router.post('/staff/create',Staff.create);
+
+router.post('/staff/create',Right.create);
+
+module.exports = router;
